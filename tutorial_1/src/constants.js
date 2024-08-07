5e0d7ef2bf8bf2ce1f9d860c61d22450fb6bce11
Carlos Alberto Arroyo Gonzalez <agonzalez.carlos@gmail.com>
2024-08-06 21:54:07 -0600

    Step Six: Check CELO Balance

--- tutorial_1/src/constants.js
+++ tutorial_1/src/constants.js
@@ -1,4 +1,4 @@
 // Change this address to yours
-export const ADDRESS = "0xd533Ca259b330c7a88f74e000a3faea2d63b7972"
+export const ADDRESS = "0xE5c68953c7Da25fdfE4E3051EAB1c6Ac5fF6F6FD"
 
 export const PLUMO_RPC_API = "https://plumo-prover-rpc.kobi.one"
